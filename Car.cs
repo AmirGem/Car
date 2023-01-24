6e6b0b5581d8a8606a776a65d7db900a95123a17
Amir.Gem <Amir.Gem@DESKTOP-RTPCI8N>
2023-01-24 11:37:58 +0330

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Car
+    {
+        public int Speed { get; set; }
+
+        public Car() 
+        {
+            Speed= 0;
+        }
+        
+        public int SpeedUp() 
+        {
+            Speed += 10;
+            return Speed;
+        }
+
+        public int SpeedDown() 
+        {
+            Speed -= 10;
+            return Speed;
+        }
+
+        public int TurnOff() 
+        {
+            Speed = 0;
+            return Speed;
+        }
+
+        public static string GetOp() 
+        {
+            return Console.ReadLine(); 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            while (true)
+            {
+                string op = Car.GetOp();
+
+                switch (op)
+                {
+                    case "+":
+                        Console.Clear();
+                        Console.WriteLine(car.SpeedUp());
+                        break;
+
+                    case "-":
+                        Console.Clear();
+                        Console.WriteLine(car.SpeedDown());
+                        break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine(car.TurnOff());
+                        break;
+                }
+            }
+        }
+    }
+}
